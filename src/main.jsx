bf5e80dd39c3ef833d582d204a5d3e2383945640
yosaka <fallingfluit@gmail.com>
2023-04-07 00:53:17 +0900

    update for resizable window

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import { appWindow } from "@tauri-apps/api/window";
+
+document
+  .getElementById("titlebar-minimize")
+  .addEventListener("click", () => appWindow.minimize());
+document
+  .getElementById("titlebar-maximize")
+  .addEventListener("click", () => appWindow.toggleMaximize());
+document
+  .getElementById("titlebar-close")
+  .addEventListener("click", () => appWindow.close());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
